892830653c4fbcc345d0a94b0b2fa6bb4af9be4e
Christian Planner <58919169+lazyevaluator@users.noreply.github.com>
2021-03-18 21:59:07 +0100

    Created chap3.hs

---
+++ chapter3/chap3.hs
@@ -0,0 +1,13 @@
+{-
+  Really nothing interesting happened in this chapter. Type System explained.
+  
+  Polymorphic Types and Overloaded Functions.
+  
+  A Polymorphic Function works on many types but has the same definition on all of them.
+  A Overloaded function may work on several types but also has different definition.
+
+  Polymorphism: one definition, many types
+  Overloading:  different definition for different types.
+
+  keyword: parametric polymorphism, see also https://www21.in.tum.de/teaching/fpv/WS20/assets/slides.pdf slide 77
+-}
